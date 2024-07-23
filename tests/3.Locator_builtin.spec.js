3e1c43b827e57c62e838d5d5d409c29833824e52
Mohan <mohankumardk98@gmail.com>
2024-07-23 16:50:03 +0530

    fixed commit

--- tests/3.Locator_builtin.spec.js
+++ tests/3.Locator_builtin.spec.js
@@ -1,11 +1,11 @@
 const { test, expect } = require('@playwright/test')//import
 test('Locator', async ({ page }) => {
   
-    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
 const logo = await page.getByAltText('company-branding')//only for alt attribute
 await expect(logo).toBeVisible()
-
+//ko
 await page.getByPlaceholder('Username').fill("Admin")//only for Placeholder attribute
 
 const by_role = await page.getByRole('button', {type:"submit"})//by role like button, link(not prefered)
@@ -13,5 +13,5 @@ await expect(by_role).toBeVisible()
 
 
 await page.getByText('Forgot your password?').click()// by linktext
-
+//mo
 })
