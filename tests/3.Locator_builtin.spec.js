35f9cc4b37ce1938eaa47d25d69186e7af8411ea
Mohan <mohankumardk98@gmail.com>
2024-07-23 16:43:55 +0530

    fixed commit

--- tests/3.Locator_builtin.spec.js
+++ tests/3.Locator_builtin.spec.js
@@ -6,7 +6,7 @@ await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/lo
 const logo = await page.getByAltText('company-branding')//only for alt attribute
 await expect(logo).toBeVisible()
 
-await page.getByPlaceholder('Username').fill("Admin")//only for Placeholder attribute
+await page.getByPlaceholder('Username').fill("Admin") //only for Placeholder attribute
 const by_role = await page.getByRole('button', {type:"submit"})//by role like button, link(not prefered)
 await expect(by_role).toBeVisible()
 await page.getByText('Forgot your password?').click()// by linktext
