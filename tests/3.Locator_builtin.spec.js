89139946c9ebf5e75afd2b19abc78c05c74ebc2f
Mohan <mohankumardk98@gmail.com>
2024-07-23 13:30:24 +0530

    abc

--- tests/3.Locator_builtin.spec.js
+++ tests/3.Locator_builtin.spec.js
@@ -10,8 +10,6 @@ await page.getByPlaceholder('Username').fill("Admin")//only for Placeholder attr
 
 const by_role = await page.getByRole('button', {type:"submit"})//by role like button, link(not prefered)
 await expect(by_role).toBeVisible()
-
-
 await page.getByText('Forgot your password?').click()// by linktext
 
 })
