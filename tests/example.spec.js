ce08b3dbfe64eebaceb1009bcbe652547fcdd372
Mohan <mohankumardk98@gmail.com>
2024-09-20 16:59:37 +0530

    fixed commit new

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,35 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require('@playwright/test')//import
+test('page image', async ({ page }) => { 
+page.goto("https://www.facebook.com/")
+await page.locator("//input[@name='email']").fill("abc")
+await page.locator("//input[@type='password']").fill("xyz")
+await page.locator("//button[@name='login']").click()
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+//npx playwright test --headed --project=chromium example.spec.js
+//npx playwright show-trace test-results\example-has-title-chromium-retry1\trace.zip
+//npx playwright codegen  
+//npx playwright install   
+//git push 
+//git commit -m "fixed commit new"      
+// git add .  
+//allure open allure-report   
+//allure generate my-allure-results -o allure-report --clean  
+//npm install -g allure-commandline --save-dev     
+//npm i -D @playwright/test allure-playwright  
+//npx playwright test --headed 23.tag.spec.js --project=chromium -g "(?=.*@smoke)(?=.*@sanity)"
+//npx playwright test --headed 23.tag.spec.js --project=chromium -g "@smoke" 
+//npx playwright show-trace test-results\12.date_picker-date-picker-chromium\trace.zip
+//npx playwright test --headed 19.group.spec.js --project=chromium -g 'begin' 
+//git stash  
+//git stash list   
+//git stash pop   
+//git fetch ex_01   
+// git merge ex_01  
+//git checkout main     
+//git branch 
+//git diff --staged    
+//git push origin main
+//git status  
+//git rebase --continue 
+//git pull --rebase origin main
+})
